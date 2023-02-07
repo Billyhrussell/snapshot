1ed8eaa1843320ef2372bc9c8df892ae2377c01e
Margaret Sun <hi.margaret.sun@gmail.com>
2023-02-07 12:14:11 -0800

    add tailwind to ImageContainer

--- snapshot/src/components/ImageContainer.js
+++ snapshot/src/components/ImageContainer.js
@@ -5,16 +5,21 @@ function ImageContainer({ imageRequest }) {
   let imageObj = imageRequest.data.photos.photo;
 
   return (
-    <>
-      {imageObj.map((element) => (
-        <Image
-          key={element.id}
-          id={element.id}
-          server={element.server}
-          secret={element.secret}
-        ></Image>
-      ))}
-    </>
+    <section class="overflow-hidden text-gray-700">
+      <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+        <div class="flex flex-wrap -m-1 md:-m-2">
+          {imageObj.map((element) => (
+            <Image
+              key={element.id}
+              id={element.id}
+              server={element.server}
+              secret={element.secret}
+            ></Image>
+          ))}
+          ;
+        </div>
+      </div>
+    </section>
   );
 }
 
